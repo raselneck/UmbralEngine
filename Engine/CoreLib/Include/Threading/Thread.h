9315bc25b69d7af7bf74f631afdf2020c7afc55a
Richard Selneck <2023410+raselneck@users.noreply.github.com>
2024-08-04 23:56:55 -0700

    Initial implementation of Thread
    Allows for basic parameterized and standalone functions to be run on
    separate threads :^)

--- Engine/CoreLib/Include/Threading/Thread.h
+++ Engine/CoreLib/Include/Threading/Thread.h
@@ -13,9 +13,54 @@ using FParameterizedThreadFunction = TFunction<void(void*)>;
 /**
  * @brief Defines a way to launch threads or manipulate the calling thread.
  */
-class FThread
+class FThread final
 {
+	UM_DISABLE_COPY(FThread);
+
+	class FThreadImpl;
+
 public:
+
+	/**
+	 * @brief Moves ownership of another thread's resources.
+	 *
+	 * @param other The other thread.
+	 */
+	FThread(FThread&& other) noexcept;
+
+	/**
+	 * @brief Destroys this thread.
+	 */
+	~FThread();
+
+	/**
+	 * @brief Creates a new thread.
+	 *
+	 * @param function The function to run on the thread.
+	 * @return The thread.
+	 */
+	static FThread Create(FThreadFunction function);
+
+	/**
+	 * @brief Creates a new thread.
+	 *
+	 * @param function The parameterized function to run on the thread.
+	 * @param parameter The parameter to pass along to \p function.
+	 * @return The thread.
+	 */
+	static FThread Create(FParameterizedThreadFunction function, void* parameter);
+
+	/**
+	 * @brief Checks to see if this thread is valid.
+	 *
+	 * @return True if this thread is valid, otherwise false.
+	 */
+	[[nodiscard]] bool IsValid() const;
+
+	/**
+	 * @brief Blocks the calling thread until this thread terminates.
+	 */
+	void Join();
 
 	/**
 	 * @brief Sleeps the calling thread for the given duration.
@@ -23,4 +68,26 @@ public:
 	 * @param duration The duration to sleep for.
 	 */
 	static void Sleep(FTimeSpan duration);
+
+	/**
+	 * @brief Moves ownership of another thread's resources.
+	 *
+	 * @param other The other thread.
+	 * @return This thread.
+	 */
+	FThread& operator=(FThread&& other) noexcept;
+
+private:
+
+	/**
+	 * @brief Sets default values for this thread's properties.
+	 */
+	FThread();
+
+	TUniquePtr<FThreadImpl> m_Impl;
+};
+
+template<>
+struct TIsZeroConstructible<FThread> : FTrueType
+{
 };
--- Engine/CoreLib/Source/Threading/Thread.cpp
+++ Engine/CoreLib/Source/Threading/Thread.cpp
@@ -1,11 +1,239 @@
+#include "Engine/Logging.h"
+#include "HAL/InternalTime.h"
 #include "Threading/Thread.h"
-#if UMBRAL_PLATFORM_IS_WINDOWS
-#	include "Threading/Windows/WindowsThread.h"
-#else
-#	include "Threading/Posix/PosixThread.h"
-#endif
+#include <pthread.h>
+
+enum class EThreadState : uint8
+{
+	WaitingToRun,
+	Running,
+	Finished,
+	Joined
+};
+
+class FThread::FThreadImpl final
+{
+public:
+
+	/**
+	 * @brief Creates a new thread implementation.
+	 *
+	 * @param function The function to run.
+	 */
+	explicit FThreadImpl(FThreadFunction function)
+		: m_Function { MoveTemp(function) }
+	{
+	}
+
+	/**
+	 * @brief Creates a new thread implementation.
+	 *
+	 * @param function The function to run.
+	 * @param parameter The parameter to pass along to the function.
+	 */
+	explicit FThreadImpl(FParameterizedThreadFunction function, void* parameter)
+		: m_ParameterizedFunction { MoveTemp(function) }
+		, m_Parameter { parameter }
+	{
+	}
+
+	/**
+	 * @brief Destroys this thread implementation.
+	 */
+	~FThreadImpl()
+	{
+		UM_ASSERT(m_State == EThreadState::Finished || m_State == EThreadState::Joined, "Destroying thread implementation without joining");
+	}
+
+	/**
+	 * @brief Attempts to join this thread.
+	 */
+	void Join()
+	{
+		if (m_State != EThreadState::Running)
+		{
+			return;
+		}
+
+		void* status = nullptr;
+		const int32 result = pthread_join(m_ThreadHandle, &status);
+
+		if (result == 0)
+		{
+			m_State = EThreadState::Joined;
+			return;
+		}
+
+		switch (result)
+		{
+		case EINVAL:  UM_LOG(Fatal, "Attempting to join thread that cannot be joined"); break;
+		case ESRCH:   UM_LOG(Fatal, "Attempting to join invalid thread"); break;
+		case EDEADLK: UM_LOG(Fatal, "Attempting to join a thread from within the thread"); break;
+		default:      UM_LOG(Fatal, "Unknown error encountered while joining thread"); break;
+		}
+	}
+
+	/**
+	 * @brief Runs this thread implementation.
+	 */
+	void Run()
+	{
+		pthread_attr_t threadAttr {};
+		pthread_attr_init(&threadAttr);
+		pthread_attr_setdetachstate(&threadAttr, PTHREAD_CREATE_JOINABLE);
+
+		const int32 result = [this, &threadAttr]()
+		{
+			if (m_Function.IsValid())
+			{
+				return pthread_create(&m_ThreadHandle, &threadAttr, RunFunction, this);
+			}
+			else
+			{
+				return pthread_create(&m_ThreadHandle, &threadAttr, RunParameterizedFunction, this);
+			}
+		}();
+
+		if (result == 0)
+		{
+			m_State = EThreadState::Running;
+			return;
+		}
+
+		switch (result)
+		{
+		case EAGAIN: UM_LOG(Fatal, "The limit on the number of threads for this process has been reached"); break;
+		case EINVAL: UM_LOG(Fatal, "Thread given invalid attributes during creation"); break;
+		case EPERM:  UM_LOG(Fatal, "Insufficient permissions to create thread"); break;
+		default:     UM_LOG(Fatal, "Unknown error encountered while creating thread"); break;
+		}
+	}
+
+private:
+
+	/**
+	 * @brief Runs m_Function on a thread implementation.
+	 *
+	 * @param opaqueImpl The thread implementation.
+	 * @return Nothing.
+	 */
+	static void* RunFunction(void* opaqueImpl)
+	{
+		FThreadImpl* impl = reinterpret_cast<FThreadImpl*>(opaqueImpl);
+		impl->m_Function.Invoke();
+		impl->m_State = EThreadState::Finished;
+
+		return nullptr;
+	}
+
+	/**
+	 * @brief Runs m_Function on a thread implementation.
+	 *
+	 * @param opaqueImpl The thread implementation.
+	 * @return Nothing.
+	 */
+	static void* RunParameterizedFunction(void* opaqueImpl)
+	{
+		FThreadImpl* impl = reinterpret_cast<FThreadImpl*>(opaqueImpl);
+		impl->m_ParameterizedFunction.Invoke(impl->m_Parameter);
+		impl->m_State = EThreadState::Finished;
+
+		return nullptr;
+	}
+
+	pthread_t m_ThreadHandle {};
+	FThreadFunction m_Function;
+	FParameterizedThreadFunction m_ParameterizedFunction;
+	void* m_Parameter = nullptr;
+	EThreadState m_State = EThreadState::WaitingToRun;
+};
+
+FThread::FThread() = default;
+
+FThread::FThread(FThread&& other) noexcept
+	: m_Impl { MoveTemp(other.m_Impl) }
+{
+	UM_ASSERT(other.m_Impl.IsNull(), "Failed to move thread resource ownership");
+}
+
+FThread::~FThread()
+{
+	if (m_Impl.IsValid())
+	{
+		m_Impl->Join();
+	}
+}
+
+FThread FThread::Create(FThreadFunction function)
+{
+	FThread thread;
+	thread.m_Impl = MakeUnique<FThreadImpl>(MoveTemp(function));
+	thread.m_Impl->Run();
+	return thread;
+}
+
+FThread FThread::Create(FParameterizedThreadFunction function, void* parameter)
+{
+	FThread thread;
+	thread.m_Impl = MakeUnique<FThreadImpl>(MoveTemp(function), parameter);
+	thread.m_Impl->Run();
+	return thread;
+}
+
+bool FThread::IsValid() const
+{
+	return m_Impl.IsValid();
+}
+
+void FThread::Join()
+{
+	UM_ASSERT(m_Impl.IsValid(), "Attempting to join invalid thread");
+
+	m_Impl->Join();
+}
+
+static struct timespec TimeSpanToTimeSpec(const FTimeSpan timeSpan)
+{
+	struct timespec duration {};
+	duration.tv_sec = static_cast<time_t>(timeSpan.GetTotalSeconds());
+
+	const FTimeSpan timeSpanWithoutSeconds = timeSpan - FTimeSpan::FromSeconds(static_cast<double>(duration.tv_sec));
+	duration.tv_nsec = static_cast<decltype(duration.tv_nsec)>(Time::TicksToNanoseconds(timeSpanWithoutSeconds.GetTicks()));
+
+	return duration;
+}
 
 void FThread::Sleep(const FTimeSpan duration)
 {
-	FPlatformThread::Sleep(duration);
+	if (duration.GetTicks() <= 0)
+	{
+		return;
+	}
+
+	struct timespec delayDuration = TimeSpanToTimeSpec(duration);
+	const int32 result = pthread_delay_np(&delayDuration);
+
+	if (result == 0)
+	{
+		return;
+	}
+
+	switch (result)
+	{
+	case EINVAL: UM_LOG(Error, "Invalid sleep interval given ({} ticks)", duration.GetTicks()); break;
+	default:     UM_LOG(Error, "Unknown error when attempting to sleep thread (error {})", result); break;
+	}
+}
+
+FThread& FThread::operator=(FThread&& other) noexcept
+{
+	if (&other == this)
+	{
+		return *this;
+	}
+
+	m_Impl = MoveTemp(other.m_Impl);
+	UM_ASSERT(other.m_Impl.IsNull(), "Failed to move thread resource ownership");
+
+	return *this;
 }
