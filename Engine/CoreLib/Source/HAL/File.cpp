548771206bc2a367b8eac0da975a6ecb44d51c12
Richard Selneck <2023410+raselneck@users.noreply.github.com>
2024-09-22 23:42:38 -0700

    Fix permissions issue when creating the file to async write lines to
    Passing in zero for `mode` in `uv_fs_open` when the file is being
    created for writing was definitely not the way to go. On Windows, that
    creates a read-only file :^)

--- Engine/CoreLib/Source/HAL/File.cpp
+++ Engine/CoreLib/Source/HAL/File.cpp
@@ -21,6 +21,24 @@
 #undef DeleteFile
 #endif
 
+#ifndef S_IRWXU
+enum : int32
+{
+	S_IRWXU = 00700, // user (file owner) has read, write, and execute permission
+	S_IRUSR = 00400, // user has read permission
+	S_IWUSR = 00200, // user has write permission
+	S_IXUSR = 00100, // user has execute permission
+	S_IRWXG = 00070, // group has read, write, and execute permission
+	S_IRGRP = 00040, // group has read permission
+	S_IWGRP = 00020, // group has write permission
+	S_IXGRP = 00010, // group has execute permission
+	S_IRWXO = 00007, // others have read, write, and execute permission
+	S_IROTH = 00004, // others have read permission
+	S_IWOTH = 00002, // others have write permission
+	S_IXOTH = 00001, // others have execute permission
+};
+#endif
+
 /**
  * @brief The maximum length for a file that we can read.
  *
@@ -501,7 +519,10 @@ public:
 
 		uv_loop_t* loop = GetEventLoop()->GetLoop();
 		uv_fs_cb callback = Super::template DispatchRequest<&ThisClass::OnOpen>;
-		uv_fs_open(loop, m_OpenRequest.Get(), m_FilePath.GetChars(), UV_FS_O_CREAT | UV_FS_O_TRUNC | UV_FS_O_WRONLY, 0, callback);
+
+		constexpr int32 openFlags = O_CREAT | O_TRUNC | O_WRONLY;
+		constexpr int32 openMode = S_IRUSR | S_IWUSR;
+		uv_fs_open(loop, m_OpenRequest.Get(), m_FilePath.GetChars(), openFlags, openMode, callback);
 	}
 
 private:
