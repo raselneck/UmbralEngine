16dcbaeda7134a94f8f47456f886fcd017c797e8
Richard Selneck <2023410+raselneck@users.noreply.github.com>
2024-09-27 00:24:22 -0700

    Support the object heap visitor visiting arrays of objects

--- Engine/ObjectLib/Include/Object/ObjectHeapVisitor.h
+++ Engine/ObjectLib/Include/Object/ObjectHeapVisitor.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "Containers/Array.h"
 #include "Containers/Span.h"
-#include "Templates/IsObject.h"
+#include "Object/ObjectPtr.h"
+#include "Object/WeakObjectPtr.h"
+#include "Templates/AndNotOr.h"
+#include "Templates/IsConvertible.h"
+#include "Templates/IsSame.h"
 
-class FObjectPtr;
-class FWeakObjectPtr;
 class UObject;
 
 /**
@@ -39,17 +42,32 @@ public:
 	 * @brief Visits all objects in a span.
 	 *
 	 * @tparam T The object type.
-	 * @param objectSpan The object span.
+	 * @param span The span of objects.
 	 */
 	template<typename T>
-	void Visit(TSpan<T> objectSpan)
+	void Visit(const TSpan<T> span)
 	{
-		static_assert(TIsObject<T>::Value);
+		static_assert(Or<
+		    TAnd<TIsBaseOf<UObject, T>, TIsPointer<T>>, // Can be a span of raw object pointers
+		    TIsConvertible<T, const FObjectPtr&>,       // Can be a span of FObjectPtr or TObjectPtr<T>
+		    TIsConvertible<T, const FWeakObjectPtr&>    // Can be a span of FWeakObjectPtr or TWeakObjectPtr<T>
+		>);
 
-		for (T& object : objectSpan)
+		for (T& object : span)
 		{
 			Visit(object);
 		}
+	}
+
+	/**
+	 * @brief Visits all objects in an object array.
+	 *
+	 * @param objects The object array to iterate.
+	 */
+	template<typename T>
+	void Visit(const TArray<T>& objects)
+	{
+		Visit(objects.AsSpan());
 	}
 
 protected:
@@ -59,3 +77,12 @@ protected:
 	 */
 	virtual ~FObjectHeapVisitor() = default;
 };
+
+template<typename T> struct TCanObjectHeapVisit : FFalseType {};
+template<> struct TCanObjectHeapVisit<UObject*> : FTrueType {};
+template<> struct TCanObjectHeapVisit<FObjectPtr> : FTrueType {};
+template<> struct TCanObjectHeapVisit<FWeakObjectPtr> : FTrueType {};
+template<typename T> struct TCanObjectHeapVisit<TObjectPtr<T>> : FTrueType {};
+template<typename T> struct TCanObjectHeapVisit<TWeakObjectPtr<T>> : FTrueType {};
+template<typename T> struct TCanObjectHeapVisit<TSpan<T>> : TCanObjectHeapVisit<T> {};
+template<typename T> struct TCanObjectHeapVisit<TArray<T>> : TCanObjectHeapVisit<T> {};
