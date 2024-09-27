f96e4ead5e9928a0c6134b0a8946683bcae4d22f
Richard Selneck <2023410+raselneck@users.noreply.github.com>
2024-09-27 01:22:53 -0700

    Remove the const overload for Forward

--- Engine/CoreLib/Include/Templates/Forward.h
+++ Engine/CoreLib/Include/Templates/Forward.h
@@ -2,39 +2,14 @@
 
 #include "Templates/IsReference.h"
 
-/**
- * @brief Forwards an l-value as an r-value.
- *
- * @tparam T The value type.
- * @param value The value to forward.
- * @return \p value as an r-value.
- */
+// https://en.cppreference.com/w/cpp/utility/forward
+
 template<typename T>
-[[nodiscard]] constexpr T&& Forward(RemoveConstReference<T>& value) noexcept
+[[nodiscard]] constexpr T&& Forward(RemoveReference<T>& value) noexcept
 {
 	return static_cast<T&&>(value);
 }
 
-/**
- * @brief Forwards an l-value.
- *
- * @tparam T The value type.
- * @param value The value to forward.
- * @return \p value as an r-value.
- */
-template<typename T>
-[[nodiscard]] constexpr const T& Forward(const RemoveReference<T>& value) noexcept
-{
-	return static_cast<const T&>(value);
-}
-
-/**
- * @brief Forwards an r-value as an r-value, and prohibits forwarding of l-values as r-values.
- *
- * @tparam T The value type.
- * @param value The value to forward.
- * @return \p value as an r-value.
- */
 template<typename T>
 [[nodiscard]] constexpr T&& Forward(RemoveReference<T>&& value) noexcept
 {
