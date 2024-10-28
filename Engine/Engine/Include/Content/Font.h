1e0ff8d2d0007b4cf0d0d55fc4ed197a89704ae2
Richard Selneck <2023410+raselneck@users.noreply.github.com>
2024-10-27 18:35:06 -0700

    Begin working on font loading and text rendering

---
+++ Engine/Engine/Include/Content/Font.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Object/Object.h"
+#include "Font.Generated.h"
+
+/**
+ * @brief Defines a font.
+ */
+UM_CLASS()
+class UFont : public UObject
+{
+	UM_GENERATED_BODY();
+};
---
+++ Engine/Engine/Include/Rendering/TextRenderer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Graphics/GraphicsResource.h"
+#include "TextRenderer.Generated.h"
+
+/**
+ * @brief Defines a helper for rendering text.
+ */
+UM_CLASS()
+class UTextRenderer : public UGraphicsResource
+{
+	UM_GENERATED_BODY();
+};
